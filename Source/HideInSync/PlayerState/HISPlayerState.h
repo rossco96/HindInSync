ff132ac69b83ecd159b71720400c4c31fa8eff1f
rossco96 <coles.ross@gmail.com>
2023-11-07 14:40:06 +0000

    LevelSettingsData and HISPlayerState (both empty)

---
+++ Source/HideInSync/PlayerState/HISPlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "HISPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HIDEINSYNC_API AHISPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
